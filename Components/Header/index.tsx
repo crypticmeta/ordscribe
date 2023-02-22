1308a0a16ba82ab3cc60ef576b2e9c035238efc2
pRay01 <pray9563@gmail.com>
2023-02-22 09:14:50 +0530

    hero page fixed with footer

--- Components/Header/index.tsx
+++ Components/Header/index.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 
  const Header = () => {
   return (
-    <div className='bg-black flex justify-between space-x-7 py-2 px-10 items-center'>
-        <div className=' lg:w-3/12' >
-        <Link href="/"> <img className='' src='/assets/images/logo.png'/> </Link>
+    <div className='bg-black flex justify-between space-x-7 py-4 px-10 items-center'>
+        <div className='lg:w-5/12 md:w-5/12  ' >
+        <Link href="/"> <img className='w-[50%]' src='/assets/images/logo.png'/> </Link>
         </div>
-        <div className='text-white  lg:text-3xl fira lg:w-5/12'>
-            <p>Bitcoin Ordinal Inscription</p>
+        <div className='text-white  flex justify-end  lg:text-3xl fira lg:w-7/12'>
+            <p className=''>Bitcoin Ordinal Inscription</p>
         </div>
     </div>
   )
